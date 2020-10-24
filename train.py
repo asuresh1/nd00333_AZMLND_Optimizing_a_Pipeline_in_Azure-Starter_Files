7e873578d972e8d6acbda228d151f7773c816be6
Suresh Aswathnarayana <suresh.aswathnarayana@live.com>
2020-10-24 19:24:09 +0000

    Updated Python Notebook with complete run

--- train.py
+++ train.py
@@ -10,15 +10,12 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
 #Use Pandas to read the file
 data_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 ### YOUR CODE HERE ###
-ds = Dataset.Tabular.from_delimited_files(data_path)
+ds = TabularDatasetFactory.from_delimited_files(data_path)
 
+ds.to_pandas_dataframe()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -30,25 +27,27 @@ def clean_data(data):
     jobs = pd.get_dummies(x_df.job, prefix="job")
     x_df.drop("job", inplace=True, axis=1)
     x_df = x_df.join(jobs)
+    #print ("I am here")
     x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
     x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
     x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
     x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
     contact = pd.get_dummies(x_df.contact, prefix="contact")
+    
     x_df.drop("contact", inplace=True, axis=1)
     x_df = x_df.join(contact)
     education = pd.get_dummies(x_df.education, prefix="education")
     x_df.drop("education", inplace=True, axis=1)
     x_df = x_df.join(education)
+    
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
-x, y = clean_data(ds)
+    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df,y_df
 
-# TODO: Split data into train and test sets.
+x, y = clean_data(ds)
 
 ### YOUR CODE HERE ###
 # Use Sklearn train_test_split with 30% data in the test set
